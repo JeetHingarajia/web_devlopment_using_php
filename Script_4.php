143c173df120c38d75455997e0065cc9dcd0935a
JeetHingarajia <132185829+JeetHingarajia@users.noreply.github.com>
2023-08-29 12:10:22 +0530

    Add files via upload
    Script_4 from Jeet Hingarajia

---
+++ Script_4.php
@@ -0,0 +1,9 @@
+
+<?php
+/*4. Write a PHP program that assigns your first name to the variable $firstName and your last name to 
+the variable $lastName. Use concatenation to display a greeting message like "Hello, John Doe!"*/
+
+$firstName="Jeet";
+$lastName="Patel";
+echo"Hello,$firstName $lastName!";
+?>
