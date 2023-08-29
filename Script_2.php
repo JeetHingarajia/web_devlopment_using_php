58cacb8784765ccf01f4f12a5af7e887075fe21f
JeetHingarajia <132185829+JeetHingarajia@users.noreply.github.com>
2023-08-29 12:08:07 +0530

    Add files via upload
    Script_2 from Jeet Hingarajia

---
+++ Script_2.php
@@ -0,0 +1,7 @@
+<?php
+/*2. Create a PHP program that uses concatenation to display the following message:
+i. My name is John, and I am a PHP developer*/
+$a="My name is John,";
+$b="I am a PHP developer";
+echo"$a.and.$b";
+?>
