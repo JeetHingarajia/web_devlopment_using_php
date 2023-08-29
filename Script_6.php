559413592d8d100ea368aa54c75df0edc6ff360c
JeetHingarajia <132185829+JeetHingarajia@users.noreply.github.com>
2023-08-29 12:26:41 +0530

    Add files via upload
    Script_6 from Jeet Hingarajia

---
+++ Script_6.php
@@ -0,0 +1,8 @@
+<?php 
+/*6. Develop a PHP program that assigns an initial value to the variable $x, then reassigns it to a new 
+value, and finally echoes the value.*/
+$x=25;
+$y=$x;
+
+echo"initial value to the variable : $y";
+?>
