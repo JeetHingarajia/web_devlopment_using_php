eafd91de1555426a72eb769d09e61cecfd7c3972
JeetHingarajia <132185829+JeetHingarajia@users.noreply.github.com>
2023-09-02 11:13:16 +0530

    Add files via upload
    Script_14 from Jeet Hingarajia

---
+++ Script_14.php
@@ -0,0 +1,16 @@
+<?php
+/*Create a PHP program that uses echo to display the following text with proper formatting:
+line1
+	line2
+		line3*/
+		
+echo"<ul>\n";
+echo"	Line 1\n";
+echo"	<ul>\n";
+echo"		Line 2\n";
+echo"		<ul>\n";
+echo"			Line 3\n";
+echo"		</ul>\n";
+echo"	</ul>\n";
+echo"</ul>\n";
+?>
