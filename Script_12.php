5f44439dfbdc70295e268698832d432598418cbc
JeetHingarajia <132185829+JeetHingarajia@users.noreply.github.com>
2023-09-02 11:12:19 +0530

    Add files via upload
    Script_12 from Jeet Hingarajia

---
+++ Script_12.php
@@ -0,0 +1,9 @@
+<?php
+/*.Develop a PHP program that uses echo to display an unordered list (<ul>) with three list items (<li>)
+containing different colors.*/
+echo"<ul>";
+echo"<li> Orange </li>";
+echo"<li> Red </li>";
+echo"<li> Yellow </li>";
+echo"</ul>";
+?>
