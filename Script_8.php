cf456f959a8402fb0f27a6da19ff2a9c978c45fb
JeetHingarajia <132185829+JeetHingarajia@users.noreply.github.com>
2023-09-02 11:09:30 +0530

    Add files via upload
    Script_8 from Jeet Hingarajia

---
+++ Script_8.php
@@ -0,0 +1,8 @@
+<?php
+/*Develop a PHP program that assigns a name to the variable $name and uses it to display a dynamic
+greeting message like "Hello, John!".*/
+$name="John";
+$greet="Hello,". $name."!";
+
+echo $greet;
+?>
