8ac693f28ff4bf3bd085e8148cb295e3e4690beb
JeetHingarajia <132185829+JeetHingarajia@users.noreply.github.com>
2023-09-02 11:11:15 +0530

    Add files via upload
    Script_10 from Jeet Hingarajia

---
+++ Script_10.php
@@ -0,0 +1,7 @@
+<?php
+/*Write a PHP program that assigns a name to the variable $name and an age to the variable $age.
+Use concatenation to display a message like "Hello, John! You are 25 years old."*/
+$name="John";
+$age="25";
+echo"Hello,".$name."! you are ".$age."years old.";
+?>
