2e63fab405b48818194d2882ad97ab7fa04f62ca
JeetHingarajia <132185829+JeetHingarajia@users.noreply.github.com>
2023-09-02 11:10:37 +0530

    Add files via upload
    Script_9 from Jeet Hingarajia

---
+++ Script_9.php
@@ -0,0 +1,10 @@
+<?php
+/*Write a PHP program that assigns a string to the variable $text and uses the strlen() function to
+display its length along with the original string.*/
+
+$text ="Jeet";
+$length=strlen($text);
+echo"Original String: ".$text."<br>";
+echo"length of String :".$length;
+
+?>
